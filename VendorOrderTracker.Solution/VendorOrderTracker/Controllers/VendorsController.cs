22fd7bb476478edd91bc30c90854227c0bd3d6b3
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-17 12:31:49 -0800

    add the ability to create and see a list of vendors

--- VendorOrderTracker.Solution/VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker.Solution/VendorOrderTracker/Controllers/VendorsController.cs
@@ -13,5 +13,17 @@ namespace VendorOrderTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor myVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
   }
 }
