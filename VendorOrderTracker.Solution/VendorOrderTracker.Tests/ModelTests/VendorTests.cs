12cc20c02637ef1f0565ac4b64ecea958f43b8fd
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-17 09:47:19 -0800

    add the vendor class and failing test

---
+++ VendorOrderTracker.Solution/VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests 
+  {
+    
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      Assert.AreEqual(typeof(Order), newVendor.GetType());
+    }
+  }
+}
